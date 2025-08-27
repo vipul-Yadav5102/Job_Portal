24b6a5446161843c0bf461208187c6f5ff2ab267
vipul-Yadav5102 <vipul.20225102@mnnit.ac.in>
2025-08-27 23:53:26 +0530

    components hero topniches section done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Jobs from "./pages/Jobs";
-import Login from "./pages/Login";
-import NotFound from "./pages/NotFound";
-import PostApplication from "./pages/PostApplication";
-import Register from "./pages/Register";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { useDispatch } from "react-redux";
-import { getUser } from "./store/slices/userSlice";
+ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+ import Navbar from "./components/Navbar";
+ import Footer from "./components/Footer";
+ import Home from "./pages/Home";
+ import Dashboard from "./pages/Dashboard";
+ import Jobs from "./pages/Jobs";
+  import Login from "./pages/Login";
+  import NotFound from "./pages/NotFound";
+// import PostApplication from "./pages/PostApplication";
+// import Register from "./pages/Register";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { useDispatch } from "react-redux";
+// import { getUser } from "./store/slices/userSlice";
 
 const App = () => {
   const dispatch = useDispatch();
